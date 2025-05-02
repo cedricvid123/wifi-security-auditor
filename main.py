7722962e92667c3206a2f5ad2f5d050a09fd9e53
cedricvid123 <85314652+cedricvid123@users.noreply.github.com>
2025-05-02 15:00:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # Entry point of the tool
+
+from scanner.wifi_scanner import scan_networks
+
+if __name__ == "__main__":
+    networks = scan_networks()
+    for net in networks:
+        print(f"SSID: {net['ssid']}, BSSID: {net['bssid']}, Channel: {net['channel']}, Encryption: {net['encryption']}, Signal: {net['signal']}")
